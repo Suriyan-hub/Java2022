8e416b9f6c2a42e657dab602b7c7d190943501ac
Suriyan-hub <58413966+Suriyan-hub@users.noreply.github.com>
2022-10-18 12:48:57 +0530

    Add files via upload

---
+++ JavaBasic/Engine.java
@@ -0,0 +1,16 @@
+package com.hasA;
+
+public class Engine {
+	public void start() 
+    { 
+        
+        System.out.println("Started:"); 
+    } 
+  
+    
+    public void stop() 
+    { 
+        
+        System.out.println("Stopped:"); 
+    } 
+}
