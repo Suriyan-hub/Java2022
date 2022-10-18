ca8e6f00e0b2310c15b2791cce3eb79b9dd5c476
Suriyan-hub <58413966+Suriyan-hub@users.noreply.github.com>
2022-10-18 12:48:25 +0530

    Add files via upload

---
+++ JavaBasic/Toyota.java
@@ -0,0 +1,13 @@
+package com.hasA;
+
+public class Toyota {
+	public void ToyotaStartDemo() 
+    { 
+        // Creating an object of Engine type 
+        // using stop() method 
+        // Here, MaseratiEngine is name of an object 
+        Engine ToyotaEngine = new Engine(); 
+        ToyotaEngine.start(); 
+        ToyotaEngine.stop(); 
+    } 
+}
