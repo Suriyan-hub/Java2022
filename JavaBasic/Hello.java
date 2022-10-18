9027ef1912cf70afec88da99b2feea007d7c4e99
Suriyan-hub <58413966+Suriyan-hub@users.noreply.github.com>
2022-10-18 11:41:32 +0530

    Create Hello.java

---
+++ JavaBasic/Hello.java
@@ -0,0 +1,7 @@
+class Hello
+{
+public static void main(String[]args)
+{
+System.out.print("hello world"); System.out.print(' ava is simple"); System.out.println("Robust"); System.out.print("Object Oriented"); System.out.println("Kompetenzen Technology");
+}
+}
