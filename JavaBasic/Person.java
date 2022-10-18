3acb54b98112f3292fcb73d7bd237e1e92f64fc6
Suriyan-hub <58413966+Suriyan-hub@users.noreply.github.com>
2022-10-18 12:12:02 +0530

    Add files via upload

---
+++ JavaBasic/Person.java
@@ -0,0 +1,15 @@
+package com.referncetypecasting;
+
+public class Person {
+		   String name;
+		   int age;
+		   public Person(String name, int age){
+		      this.name = name;
+		      this.age = age;
+		   }
+		   public void displayPerson() {
+		      System.out.println("Name: "+this.name);
+		      System.out.println("Age: "+this.age);
+		   }
+		}
+
